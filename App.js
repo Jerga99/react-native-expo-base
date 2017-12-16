a058d8bb69b12abac663fa791c4cf1b3af40d4fb
Filip Jerga <filipjerga@Filips-MacBook-Pro.local>
2017-12-16 12:45:27 +0100

    Add ts

--- App.js
+++ App.js
@@ -1,35 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './src/screens/LoginScreen';
-import { CaptureTimeScreen } from './src/screens/CaptureTimeScreen';
-import { TabNavigator } from 'react-navigation';
-import store from './src/store';
-import { Provider } from 'react-redux';
+import App from './src/App'
 
-export default class App extends React.Component {
-  render() {  
-    const MainNavigator = TabNavigator({
-      login: { screen: LoginScreen},
-      main: {
-        screen: TabNavigator({
-          time: { screen: CaptureTimeScreen}
-        })
-      }
-    });
-
-    return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          <MainNavigator/>
-        </View>
-      </Provider>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-});
+export default App
